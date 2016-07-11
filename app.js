d30a7c86f141d1e9d3b1d071400d3661bf30cdcd
Chris Chen <cschen13@gmail.com>
2016-07-11 18:34:52 -0400

    Updated a lot of components; Firebase ready; mobile-responsive; pulls from Firebase Database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var garageApp = angular.module('garageApp', [
 	'ngMaterial',
 	'ngRoute',
 	'homeController',
-	'residentController'
+	'residentController',
 ]);
 
 garageApp.config(['$routeProvider', '$mdThemingProvider',
@@ -25,15 +25,16 @@ garageApp.config(['$routeProvider', '$mdThemingProvider',
 			.accentPalette('pink');
 	}]);
 
+// This service is used to pass the selected group from MainCtrl to HomeCtrl.
 garageApp.service('groupService', function() {
-	var currentGroup = { name: '', members: []};
+	var selectedGroup = { name: '', members: []};
 
 	var get = function() {
-		return currentGroup;
+		return selectedGroup;
 	};
 
 	var set = function(g) {
-		currentGroup = g;
+		selectedGroup = g;
 	};
 
 	return {
@@ -42,6 +43,8 @@ garageApp.service('groupService', function() {
 	};
 });
 
+// Likely isn't necessary now that I can query the database.
+// Take out in future commit.
 garageApp.service('directoryService', function() {
 	var allGroups = {};
 
@@ -54,67 +57,249 @@ garageApp.service('directoryService', function() {
 	};
 
 	return { set: set, get: get};
-})
+});
 
 garageApp.controller('MainCtrl', ['$rootScope', '$scope', 'groupService',
-	'directoryService',
-	function($rootScope, $scope, groupService, directoryService) {
-		$scope.currentGroup = { name: '', members: []};
+	'directoryService', '$mdDialog', '$mdSidenav', '$mdMedia',
+	function($rootScope, $scope, groupService, directoryService, $mdDialog, $mdSidenav, $mdMedia) {
+		$scope.selectedGroup = { name: '', members: []};
+		$scope.menu = {
+			sections: {}
+		};
+		$scope.members = {};
 
+		// Basically grabbing all of the data from the database one time 
+		// when the page first loads.
+		// Could get hairy if too many people in the database. Might need
+		// to put in some sort of loading indicator in the future.
+		firebase.database().ref('/teams').once('value').then(function(snapshot) {
+			var newTeams = snapshot.val();
+			firebase.database().ref('/staff').once('value').then(function(snapshot) {
+				var newStaff = snapshot.val();
+				// $apply needed to update the DOM.
+				$scope.$apply(function() {
+					$scope.menu.sections = { newTeams, newStaff };
+				});
+
+				// Send broadcast so that HomeCtrl knows to pull the info.
+				$rootScope.$broadcast('GROUPS_LOADED', $scope.menu);
+				firebase.database().ref('/members').once('value').then(function(snapshot) {
+					var members = snapshot.val();
+					$scope.$apply(function() {
+						$scope.members = members;
+					});
+
+					$rootScope.$broadcast('MEMBERS_LOADED', $scope.members);
+				});
+			});
+		});
+
+		// Called whenever a user selects a group from the sidenav.
 		$scope.setGroup = function(g) {
 			groupService.set(g);
-			$scope.currentGroup = groupService.get();
-			$rootScope.$broadcast('GROUP_CHANGED', $scope.currentGroup);
+			$scope.selectedGroup = groupService.get();
+			$rootScope.$broadcast('GROUP_CHANGED', $scope.selectedGroup);
+			// If you selected from a slide-out sidenav, close the sidenav.
+			if (!$mdMedia('gt-sm')) {
+				$mdSidenav('left').toggle();
+			}
 		};
 
-		// Mocks
-		var audiovert = {
-			name: 'Audiovert',
-			members: [{name: 'Chris Chen', studentID: 2846283}]
+		$scope.openMenu = function() {
+			$mdSidenav('left').toggle();
 		};
 
-		var mdar = {
-			name: 'MDAR',
-			members: [{name:'Ben Williams'}]
-		};
+		// Authentication would allow database edits right in the web app
+		// for privileged users. The login dialog will be the starting point.
+		// $scope.showLoginDialog = function() {
+		// 	$mdDialog.show({
+		// 		contentElement: '#loginDialog',
+		// 		parent: angular.element(document.body),
+		// 		clickOutsideToClose: true
+		// 	});
+		// };
 
-		var noteshark = {
-			name: 'Noteshark',
-			members: [{name:'Wyatt Cook'}, {name: 'Justin Fleishmann'}]
-		};
+		// $scope.closeDialog = function() {
+		// 	$mdDialog.hide();
+		// }
 
-		var epic = {
-			name: 'EPIC',
-			members: [{name: 'Garret Goehring'}, {name: 'Terence Chan'}, {name: 'Diane Liu'}]
-		};
+		////////////////MOCKS//////////
+		// var data = {
+		//   members : {
+		//     "aalexander" : {
+		//       "groups" : {
+		//         "audiovert" : true
+		//       },
+		//       "name" : "Ahren Alexander"
+		//     },
+		//     "bbanks" : {
+		//       "groups" : {
+		//         "fte" : true
+		//       },
+		//       "name" : "Billy Banks"
+		//     },
+		//     "bwilliams" : {
+		//       "groups" : {
+		//         "fte" : true
+		//       },
+		//       "name" : "Ben Williams"
+		//     },
+		//     "cchen" : {
+		//       "groups" : {
+		//         "aides" : true
+		//       },
+		//       "name" : "Chris Chen"
+		//     },
+		//     "csteiner" : {
+		//       "groups" : {
+		//         "eir" : true
+		//       },
+		//       "name" : "Chris Steiner"
+		//     },
+		//     "dliu" : {
+		//       "groups" : {
+		//         "epic" : true
+		//       },
+		//       "name" : "Diane Liu"
+		//     },
+		//     "emitchell" : {
+		//       "groups" : {
+		//         "fte" : true
+		//       },
+		//       "name" : "Elisa Mitchell"
+		//     },
+		//     "gcaniglia" : {
+		//       "groups" : {
+		//         "aides" : true
+		//       },
+		//       "name" : "Gabriel Caniglia"
+		//     },
+		//     "jfleishmann" : {
+		//       "groups" : {
+		//         "audiovert" : true
+		//       },
+		//       "name" : "Justin Fleishmann"
+		//     },
+		//     "jshi" : {
+		//       "groups" : {
+		//         "epic" : true
+		//       },
+		//       "name" : "Joshua Shi"
+		//     },
+		//     "mcrounse" : {
+		//       "groups" : {
+		//         "fte" : true
+		//       },
+		//       "name" : "Melissa Crounse"
+		//     },
+		//     "msaunders" : {
+		//       "groups" : {
+		//         "eir" : true
+		//       },
+		//       "name" : "Michael Saunders"
+		//     }
+		//   },
+		//   staff : {
+		//     "groups" : {
+		//       "aides" : {
+		//         "members" : {
+		//           "cchen" : true,
+		//           "gcaniglia" : true
+		//         },
+		//         "name" : "Student Services Aides"
+		//       },
+		//       "eir" : {
+		//         "members" : {
+		//           "csteiner" : true,
+		//           "msaunders" : true
+		//         },
+		//         "name" : "Entrepreneurs-in-Residence"
+		//       },
+		//       "fte" : {
+		//         "members" : {
+		//           "bbanks" : true,
+		//           "bwilliams" : true,
+		//           "emitchell" : true,
+		//           "mcrounse" : true
+		//         },
+		//         "name" : "Full-Time Employees"
+		//       }
+		//     },
+		//     "name" : "The Garage Staff"
+		//   },
+		//   teams : {
+		//     "groups" : {
+		//       "audiovert" : {
+		//         "members" : {
+		//           "aalexander" : true,
+		//           "jfleishmann" : true
+		//         },
+		//         "name" : "Audiovert"
+		//       },
+		//       "epic" : {
+		//         "members" : {
+		//           "dliu" : true,
+		//           "jshi" : true
+		//         },
+		//         "name" : "EPIC"
+		//       }
+		//     },
+		//     "name" : "Resident Teams"
+		//   }
+		// };
+		// $scope.menu.sections = [data.teams, data.staff];
+		// $scope.members = [ data.members ];
+		// $rootScope.$broadcast('GROUPS_LOADED', $scope.menu);
+		// $rootScope.$broadcast('MEMBERS_LOADED', $scope.members);
+		
 
-		var fte = {
-			name: 'Full-Time Employees',
-			members: [{name: 'Melissa Crounse'}, {name: 'Elisa Mitchell'}, {name: 'Ben Williams'}]
-		};
+		/////////////////////////////////// Old Mock
+		// var audiovert = {
+		// 	name: 'Audiovert',
+		// 	members: [{name: 'Chris Chen', studentID: 2846283}]
+		// };
 
-		var aides = {
-			name: 'Student Services Aides',
-			members: [{name: 'Chris Chen'}, {name:'Ryan Miller'}, {name: 'Joanna Li'}, {name:'Lejia Duan'}, {name:'Gabriel Caniglia'}, {name:'Brigitte Brozsus'}]
-		};
+		// var mdar = {
+		// 	name: 'MDAR',
+		// 	members: [{name:'Ben Williams'}]
+		// };
 
-		var eir = {
-			name: 'Entrepreneurs in Residence',
-			members: [{name: 'Chris Steiner'}, {name:'Ben Vear'}]
-		};
+		// var noteshark = {
+		// 	name: 'Noteshark',
+		// 	members: [{name:'Wyatt Cook'}, {name: 'Justin Fleishmann'}]
+		// };
 
-		var teams = {
-			heading: 'Resident Teams',
-			groups: [audiovert, mdar, noteshark, epic]
-		};
+		// var epic = {
+		// 	name: 'EPIC',
+		// 	members: [{name: 'Garret Goehring'}, {name: 'Terence Chan'}, {name: 'Diane Liu'}]
+		// };
 
-		var staff = {
-			heading: 'The Garage Staff',
-			groups: [fte, aides, eir]
-		};
-		$scope.menu = {
-			sections: {teams, staff}
-		};
+		// var fte = {
+		// 	name: 'Full-Time Employees',
+		// 	members: [{name: 'Melissa Crounse'}, {name: 'Elisa Mitchell'}, {name: 'Ben Williams'}]
+		// };
 
-		directoryService.set($scope.menu);
-	}]);
+		// var aides = {
+		// 	name: 'Student Services Aides',
+		// 	members: [{name: 'Chris Chen'}, {name:'Ryan Miller'}, {name: 'Joanna Li'}, {name:'Lejia Duan'}, {name:'Gabriel Caniglia'}, {name:'Brigitte Brozsus'}]
+		// };
+
+		// var eir = {
+		// 	name: 'Entrepreneurs in Residence',
+		// 	members: [{name: 'Chris Steiner'}, {name:'Ben Vear'}]
+		// };
+
+		// var teams = {
+		// 	heading: 'Resident Teams',
+		// 	groups: [audiovert, mdar, noteshark, epic]
+		// };
+
+		// var staff = {
+		// 	heading: 'The Garage Staff',
+		// 	groups: [fte, aides, eir]
+		// };
+
+		// Is this service even necessary anymore?
+		// directoryService.set($scope.menu);
+	}
+]);
--- homeView/HomeCtrl.js
+++ homeView/HomeCtrl.js
@@ -1,14 +1,79 @@
-var homeController = angular.module('homeController', ['garageApp']);
+var homeController = angular.module('homeController', ['garageApp', 'memberFilter', 'groupFilter']);
 
 homeController.controller('HomeCtrl', ['$scope', 'groupService', 'directoryService',
+	'memberFilter', 'groupFilter',
 	function($scope, groupService, directoryService) {
-		$scope.currentGroup = groupService.get();
+		// $scope.allGroupNames = [];
+		$scope.allMembers = {};
+		$scope.selectedGroup = groupService.get();
 		$scope.query = '';
 		$scope.$on('GROUP_CHANGED', function(event, newGroup) {
-			$scope.currentGroup = newGroup;
+			$scope.selectedGroup = newGroup;
 			// $scope.query = newGroup.name;
 		})
-		$scope.allGroups = directoryService.get();
+
+		$scope.$on('GROUPS_LOADED', function(event, groups) {
+			// $scope.allGroups = groups;
+			$scope.allGroups = groups.sections;
+			// $scope.$apply(function() {
+			// 	for (section in $scope.allGroups.sections) {
+			// 		var s = $scope.allGroups.sections[section];
+			// 		for (group in s.groups) {
+			// 			$scope.allGroupNames.push(s.groups[group].name);
+			// 		}
+			// 	}
+			// });
+		});
+
+		$scope.$on('MEMBERS_LOADED', function(event, members) {
+			$scope.$apply(function() {
+				$scope.allMembers = members;
+			});
+		});
+
+		$scope.isEmpty = function(obj) {
+			for (var prop in obj) {
+				if (obj.hasOwnProperty(prop))
+					return false;
+			}
+			return true && JSON.stringify(obj) === JSON.stringify({});
+		};
 
 		// $scope.test = 'This is a test.';
 	}]);
+
+// Custom filters allow me to filter the Firebase's JSON data without 
+// worrying about turning everything into an array for Angular's built-in 
+// filter.
+angular.module('groupFilter',[])
+.filter('group', function() {
+	return function(input, groupName) {
+		if (!input) return input;
+		if (!groupName) return input;
+		var expected = ('' + groupName).toLowerCase();
+		var result = {};
+		angular.forEach(input, function(actualGroup, groupKey) {
+			var actual = ('' + actualGroup.name).toLowerCase();
+			if (actual.indexOf(expected) !== -1) {
+				result[groupKey] = actualGroup;
+			}
+		});
+		return result;
+  	}
+});
+
+angular.module('memberFilter',[])
+.filter('member', function() {
+ 	return function(input, name, group) {
+		if (!input) return input;
+		var expected = ('' + name).toLowerCase();
+		var result = {};
+		angular.forEach(input, function(value, key) {
+		  var actual = ('' + value.name).toLowerCase();
+		  if (actual.indexOf(expected) !== -1 && (key in group.members) && group.members[key] == true) {
+			result[key] = value;
+		  }
+		});
+		return result;
+  	}
+});
